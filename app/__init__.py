145581b8e3e6af0f2411a56025d3ec8b835d3bec
Jaxon Rainbow <jdrainbow@waimea.school.nz>
2026-08-19 09:29:12 +1200

    started work on user form

--- app/__init__.py
+++ app/__init__.py
@@ -113,7 +113,50 @@ def add_a_recipe():
         flash(f"Club '{title}' added", "success")
         return redirect("/")
 
+#-----------------------------------------------------------
+# Add new user page
+#-----------------------------------------------------------
+@app.get("/form/add/user")
+def add_a_user_form():
+   return render_template("pages/add_user.jinja")
 
+
+#-----------------------------------------------------------
+# Route for adding a user, using data posted from a form
+#-----------------------------------------------------------
+@app.post("/add/user")
+def add_a_user():
+    # Get the data from the form
+    email  = request.form.get("email")
+    password = request.form.get("password")
+    display_name = request.form.get("meal_type")
+    
+
+    # Get the file selected via the form
+    image = request.files.get('image', None)
+    if not image or image.filename == '':
+        flash("There was a problem uploading the image", "error")
+        return redirect("/form/add/recipe")
+
+    # Sanitise filename and make it unique
+    filename = secure_filename(image.filename)
+    random_prefix = uuid.uuid4().hex[:12]
+    unique_filename = f"{random_prefix}_{filename}"
+
+    # Get the path of the upload folder
+    filepath = os.path.join(UPLOAD_FOLDER, unique_filename)
+
+    # Save file to disk
+    image.save(filepath)
+
+    # Add the form data and the upload filename to the DB
+    with connect_db() as db:
+        sql = "INSERT INTO recipes (title, url, meal_type, image_path) VALUES (?, ?, ?, ?)"
+        params = (title,url, meal_type, unique_filename)
+        db.execute(sql, params)
+# TODO make household_id so recipe upload works
+        flash(f"Club '{title}' added", "success")
+        return redirect("/")
 
 #===========================================================
 # Configure the app
